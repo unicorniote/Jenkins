b34088e06fbc9432b769b8d6656c3ea3d9b675cf
unicorniote <72157764+unicorniote@users.noreply.github.com>
2022-10-10 19:34:51 +0200

    commit prueba tarea06

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola a todos - Compilacion al hacer commit v8");
+		System.out.println("Hola a todos - Compilacion al hacer commit v8 ");
 	}
 
 }
