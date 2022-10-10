35a2ff0a11cdf80330040cd7f58ab226755d8e6b
unicorniote <72157764+unicorniote@users.noreply.github.com>
2022-10-10 19:48:21 +0200

    commit rama development

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola a todos - Rama developtment v2");
+		System.out.println("Hola a todos - Rama developtment v18278236");
 	}
 
 }
